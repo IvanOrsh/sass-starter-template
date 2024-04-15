abe5188d22f631e7fc7a549548aec6e5489b8998
IvanOrsh <ksironov@mail.ru>
2024-04-16 00:01:16 +0300

    build: initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: resolve(__dirname, "src/"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "src", "index.html"),
+        about: resolve(__dirname, "src", "about", "index.html"),
+      },
+    },
+  },
+});
