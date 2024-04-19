60e05c495569af925f08ad527355f07758602fd8
IvanOrsh <ksironov@mail.ru>
2024-04-19 23:53:17 +0300

    build: implement postcss-preset-env

---
+++ postcss.config.mjs
@@ -0,0 +1,13 @@
+import postcssPresetEnv from "postcss-preset-env";
+
+export default {
+  plugins: [
+    postcssPresetEnv({
+      stage: 3,
+      features: {
+        "trigonometric-functions": true,
+        "logical-properties-and-values": false,
+      },
+    }),
+  ],
+};
